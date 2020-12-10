15ee91e965d3253438beb541637d0a8fefc498c1
cristina.racovita98@gmail.com <cristina.racovita98@gmail.com>
2020-12-10 23:58:44 +0200

    implement login & registration activities

---
+++ UsToYou/app/src/main/java/com/example/ustoyou/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.example.ustoyou
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }, 1500)
+    }
+}
