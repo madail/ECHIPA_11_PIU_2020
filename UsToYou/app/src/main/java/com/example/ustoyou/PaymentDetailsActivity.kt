b5d685589fad0086302032dbeb3ae574f9530a11
danielgherghel98 <62747318+danielgherghel98@users.noreply.github.com>
2021-01-10 17:23:18 +0200

    Card details page (#6)

---
+++ UsToYou/app/src/main/java/com/example/ustoyou/PaymentDetailsActivity.kt
@@ -0,0 +1,21 @@
+package com.example.ustoyou
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class PaymentDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_details)
+    }
+
+    fun next(view: View) {
+
+    }
+
+    fun back(view: View) {
+
+    }
+}
