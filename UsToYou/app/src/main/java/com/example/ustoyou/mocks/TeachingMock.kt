3ce51a56722d7eb86af8cc33271f17590f3cfdfe
mada_i <iloiealexandra@yahoo.com>
2021-01-13 06:13:23 +0200

    Added virtual lesson mock in my orders

--- UsToYou/app/src/main/java/com/example/ustoyou/mocks/TeachingMock.kt
+++ UsToYou/app/src/main/java/com/example/ustoyou/mocks/TeachingMock.kt
@@ -1,6 +1,19 @@
-//package com.example.ustoyou.mocks
-//
-//class TeachingMock : IMockOrder {
-//    private var teacherName: String = "John Doe"
-//    private var date:
-//}
+package com.example.ustoyou.mocks
+
+class TeachingMock : IMockOrder {
+    private var teacherName: String = "John Doe"
+    private var date: String = "14/01/2021 14:30"
+    private var type: String = "Virtual"
+    private var paymentType: String = "Credit Card"
+    private var lessonFinished: Boolean = false
+    private var showLink: Boolean = true
+
+    private object INSTANCE {
+        val INSTANCE = TeachingMock()
+    }
+
+    companion object {
+        val instance: TeachingMock by lazy { INSTANCE.INSTANCE }
+    }
+
+}
--- UsToYou/app/src/main/java/com/example/ustoyou/model/Orders.kt
+++ UsToYou/app/src/main/java/com/example/ustoyou/model/Orders.kt
@@ -1,8 +1,16 @@
 package com.example.ustoyou.model
 
+import com.example.ustoyou.R
+import com.example.ustoyou.mocks.TeachingMock
+
 class Orders() {
     companion object {
         var orders: ArrayList<Order> = ArrayList();
+
+        init {
+            orders.add(0, Order("teaching", "Demo Virtual Lesson",
+                R.drawable.teaching_service_1, TeachingMock.instance))
+        }
 
         fun addOrder(order: Order) {
             orders.add(order)
