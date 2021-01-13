e610aede8c8384cf0914199026a74706f8ca7a7d
Mădă Iloie <48857645+madail@users.noreply.github.com>
2021-01-13 06:01:46 +0200

    Working flow between virtual lesson views (#26)

--- UsToYou/app/src/main/java/com/example/ustoyou/VirtualLesson/VirtualLessonRateActivity.kt
+++ UsToYou/app/src/main/java/com/example/ustoyou/VirtualLesson/VirtualLessonRateActivity.kt
@@ -1,19 +1,27 @@
 package com.example.ustoyou.VirtualLesson
 
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.RatingBar
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ustoyou.R
 
 
 class VirtualLessonRateActivity : AppCompatActivity() {
-    private lateinit var virtualLessonTeacherRating: RatingBar;
-    private lateinit var virtualLessonContentRating: RatingBar;
-    private lateinit var virtualLessonServiceRating: RatingBar;
-    private lateinit var virtualLessonMessageTeacherInput: EditText;
+    private lateinit var virtualLessonTeacherRating: RatingBar
+    private lateinit var virtualLessonContentRating: RatingBar
+    private lateinit var virtualLessonServiceRating: RatingBar
+    private lateinit var virtualLessonMessageTeacherInput: EditText
+
+    private lateinit var teacherName: String
+    private lateinit var date: String
+    private lateinit var type: String
+    private lateinit var paymentType: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,12 +33,80 @@ class VirtualLessonRateActivity : AppCompatActivity() {
         virtualLessonMessageTeacherInput  = findViewById(R.id.virtualLessonMessageTeacherInput)
 
         supportActionBar?.title = "Rate your lesson"
+
+        bindItems()
+
+        getExtras()
+    }
+
+    private fun bindItems() {
+        virtualLessonTeacherRating  = findViewById(R.id.virtualLessonTeacherRating)
+        virtualLessonContentRating  = findViewById(R.id.virtualLessonContentRating)
+        virtualLessonServiceRating  = findViewById(R.id.virtualLessonServiceRating)
+
+        virtualLessonMessageTeacherInput  = findViewById(R.id.virtualLessonMessageTeacherInput)
+    }
+
+    private fun getExtras() {
+        teacherName = intent.getStringExtra("teacher_name").toString()
+        if(teacherName.isEmpty()){
+            teacherName = "Teacher name"
+        }
+
+        date = intent.getStringExtra("date").toString()
+        if(teacherName.isEmpty()){
+            date = "14/01/2021 14:30"
+        }
+
+        type = intent.getStringExtra("type").toString()
+        if(teacherName.isEmpty()){
+            type = "Virtual"
+        }
+
+        paymentType = intent.getStringExtra("paymentType").toString()
+        if(teacherName.isEmpty()){
+            paymentType = "Credit Card"
+        }
     }
 
     fun onSaveRating(view: View) {
+        onLeave()
+    }
 
-
+    private fun onLeave() {
         Toast.makeText(this@VirtualLessonRateActivity, virtualLessonMessageTeacherInput.text.toString(), Toast.LENGTH_SHORT)
+
+        val intent  = Intent(this, VirtualLessonOrderDetailsActivity::class.java).apply {
+            putExtra("teacherName", teacherName)
+            putExtra("date", date)
+            putExtra("type", type)
+            putExtra("paymentType", paymentType)
+            putExtra("contentRating", virtualLessonContentRating.rating)
+            putExtra("teacherRating", virtualLessonTeacherRating.rating)
+            putExtra("serviceRating", virtualLessonServiceRating.rating)
+            putExtra("lessonFinished", true)
+            putExtra("showLink", false)
+        }
+        startActivity(intent)
+        finish()
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+
+        lateinit var dialog: AlertDialog
+
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this@VirtualLessonRateActivity)
+        builder.setTitle("Confirmation")
+        builder.setMessage("Are you sure you want to leave rating? You will not be able to return.")
+        builder.setPositiveButton("Leave") { _: DialogInterface, _: Int ->
+            onLeave()
+        }
+        builder.setNegativeButton("Cancel") { _: DialogInterface, _: Int ->
+            dialog.dismiss()
+        }
+        dialog = builder.create()
+        dialog.show()
     }
 
 }
